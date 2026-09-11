db0650b0cc56a3d543d629f990aa77fb0edffdfb
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-09-11 20:36:50 +0200

    Testing code

--- MethodOverridingApp/Program.cs
+++ MethodOverridingApp/Program.cs
@@ -10,6 +10,48 @@ namespace MethodOverridingApp
     {
         static void Main(string[] args)
         {
+            Person person = new Person();
+
+            person.FirstName = "Benjamin";
+            person.LastName = "Naidoo";
+
+            Console.WriteLine($"Person: {person}");
+
+            Employee employee = new Employee();
+            employee.FirstName = "Tim";
+            employee.LastName = "Corey";
+            employee.EmployeeNumber = 1;
+
+            Console.WriteLine($"Employee: {employee}");
+        }
+
+        public abstract class Person
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public override string ToString()
+            {
+                return $"{FirstName} {LastName}";
+            }
+
+            public abstract void TestMethod();
+        }
+
+        public class Employee : Person
+        {
+            public int EmployeeNumber { get; set; }
+
+            public override void TestMethod()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override string ToString()
+            {
+                return EmployeeNumber + " : " +base.ToString();
+            }
         }
     }
 }
